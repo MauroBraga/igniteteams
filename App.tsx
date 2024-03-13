56d0e55039df8fe01663b1e6dbafa644549b2e29
mauro <mrb0305@gmail.com>
2024-03-13 11:04:41 -0300

    fontes personalisadas

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components/native';
-
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
-
 import theme from './src/theme';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-    <Groups />
+    { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
   </ThemeProvider>
   );
 }
