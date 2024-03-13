4d1605f8d90990e4dfdfd49bfee93c524b9c7660
mauro <mrb0305@gmail.com>
2024-03-13 19:11:29 -0300

    Estilisando StatusBar

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
 import theme from './src/theme';
-import { ActivityIndicator } from 'react-native';
+import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
@@ -10,7 +11,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-    { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
-  </ThemeProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent  
+      />
+      { fontsLoaded ? <Groups/> : <Loading /> }
+    </ThemeProvider>
   );
 }
