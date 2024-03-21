f540493a48141050f1e8d5ac851b1082885d9a81
mauro <mrb0305@gmail.com>
2024-03-21 10:51:40 -0300

    Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,14 +4,16 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export  function Groups() {
 
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
-      <Header showBackButton/>
+      <Header />
       <Highlight 
         title="Turmas"
         subtitle="jogue com a sua turma"
@@ -25,8 +27,12 @@ export  function Groups() {
             title={item} 
           />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
 
+      <Button title='Criar nova turma'/>    
         
     </Container>
      
