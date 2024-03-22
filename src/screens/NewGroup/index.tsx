58ed600c8f7794adf96bd7c118356f01bcd08daf
mauro <mrb0305@gmail.com>
2024-03-22 08:49:56 -0300

    Criando componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,8 @@
 import { Header } from '@components/Header';
 import { Container, Content, Icon} from './styles';
 import { Highlight } from '@components/Highlight';
-import { Button } from 'react-native';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 export  function NewGroup() {
 
@@ -18,9 +19,11 @@ export  function NewGroup() {
             title="Nova turma"
             subtitle="crie a turma para adicionar as pessoas"
           />
+           <Input />
 
           <Button 
             title="Criar"
+            style={{ marginTop: 20 }}
           />
         </Content>
      
